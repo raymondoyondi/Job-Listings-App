fc533e8bc152532cf5d0d2eb6a1aa7073d5ddbe6
Raymond Oyondi <raymondoyondi@gmail.com>
2026-02-05 09:30:26 -0600

    Create job.model.ts

---
+++ src/app/models/job.model.ts
@@ -0,0 +1,15 @@
+export interface JobModel{
+    id: number;
+    company: string;
+    logo: string;
+    new: boolean;
+    featured: boolean;
+    position: string;
+    role: string;
+    level: string;
+    postedAt: string;
+    contract: string;
+    location: string;
+    languages?: string[] | undefined | null;
+    tools?: string[] | undefined | null;
+}
